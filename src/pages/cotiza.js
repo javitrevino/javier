faa95c1a53c0537ad1ec4889520d2c5741a32a72
javitrevino <eduardotrev91@gmail.com>
2018-11-10 13:09:59 -0600

    set netlify to true
    test

--- src/pages/cotiza.js
+++ src/pages/cotiza.js
@@ -64,7 +64,7 @@ const ContactPage = ({ data }) => (
         </div >
 
         <FormTitle> Cuentanos acerca de tu proyecto </FormTitle>
-        <form className="Form" name="cotizaciones" method="POST" netlify>
+        <form className="Form" name="cotizaciones" method="POST" netlify="true">
             <p>
                 <label>Nombre
                     <input type="text" name="name" /></label>
