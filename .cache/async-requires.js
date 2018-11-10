865e1de353c8501378b473affe7b9b0bf42d3472
javitrevino <eduardotrev91@gmail.com>
2018-11-10 12:25:22 -0600

    almost done

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,14 +2,12 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("/Users/javier/portfolio2018/javier/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/javier/portfolio2018/javier/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-dispoable-js": () => import("/Users/javier/portfolio2018/javier/src/pages/dispoable.js" /* webpackChunkName: "component---src-pages-dispoable-js" */),
+  "component---src-pages-cotiza-js": () => import("/Users/javier/portfolio2018/javier/src/pages/cotiza.js" /* webpackChunkName: "component---src-pages-cotiza-js" */),
   "component---src-pages-index-js": () => import("/Users/javier/portfolio2018/javier/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-portfolio-js": () => import("/Users/javier/portfolio2018/javier/src/pages/portfolio.js" /* webpackChunkName: "component---src-pages-portfolio-js" */),
   "component---src-pages-project-js": () => import("/Users/javier/portfolio2018/javier/src/pages/project.js" /* webpackChunkName: "component---src-pages-project-js" */),
-  "component---src-pages-servicios-js": () => import("/Users/javier/portfolio2018/javier/src/pages/servicios.js" /* webpackChunkName: "component---src-pages-servicios-js" */),
-  "component---src-pages-cotiza-js": () => import("/Users/javier/portfolio2018/javier/src/pages/cotiza.js" /* webpackChunkName: "component---src-pages-cotiza-js" */)
+  "component---src-pages-servicios-js": () => import("/Users/javier/portfolio2018/javier/src/pages/servicios.js" /* webpackChunkName: "component---src-pages-servicios-js" */)
 }
 
 exports.data = () => import("/Users/javier/portfolio2018/javier/.cache/data.json")
